e367c2262595af5e5740a1e06b65ef1e6fccd449
abdemirza <abdemirza@gmail.com>
2020-04-21 23:46:47 +0530

    Added Cards and the countUp module to display the real data

--- covid-tracker/src/components/Cards/Cards.jsx
+++ covid-tracker/src/components/Cards/Cards.jsx
@@ -1,8 +1,45 @@
-import React from  'react';
-const Cards= (props) =>{
-    console.log(props);
-    return(
-        <h1>Cards</h1>
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import CountUp from 'react-countup';
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+    if (!confirmed)
+        return 'Loading...';
+
+    return (
+        <div className="styles.container">
+            <Grid container spacing={3} justify="center">
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                        <Typography color="textSecondary" variant="h5">
+                            <CountUp start={0} end={confirmed.value}
+                                duration={2.5} separator="," />
+                        </Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">Number of Active Cases</Typography>
+
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                        <Typography color="textSecondary" variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">Number of Active Cases</Typography>
+
+                    </CardContent>
+                </Grid>
+                <Grid item component={Card}>
+                    <CardContent>
+                        <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                        <Typography color="textSecondary" variant="h5">Real Data</Typography>
+                        <Typography color="textSecondary">Real Data</Typography>
+                        <Typography variant="body2">Number of Active Cases</Typography>
+
+                    </CardContent>
+                </Grid>
+            </Grid>
+        </div>
     )
 }
 export default Cards;
